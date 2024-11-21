5d8ebfa8c5d9aca66edd783ac0a4630cf677aae9
Jillian <jilliansabrina@gmail.com>
2024-11-21 03:03:58 -0500

    Cleanup

--- server/index.ts
+++ server/index.ts
@@ -490,10 +490,6 @@ app.delete("/api/posts/:postId/like", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
